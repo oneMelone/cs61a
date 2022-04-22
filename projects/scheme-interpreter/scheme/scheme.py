bb3b0b268941d08aeb5055982c5c67dcae36367e
oneMelone <1040835678@qq.com>
2022-04-22 18:26:39 +0800

    scheme interpreter problem 4 solved

--- projects/scheme-interpreter/scheme/scheme.py
+++ projects/scheme-interpreter/scheme/scheme.py
@@ -36,6 +36,11 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 4
         "*** YOUR CODE HERE ***"
+        operator = scheme_eval(first, env)
+        validate_procedure(operator)
+        
+        operands = rest.map(lambda x: scheme_eval(x, env))
+        return scheme_apply(operator, operands, env)
         # END PROBLEM 4
 
 def self_evaluating(expr):
